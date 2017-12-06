18f0f881e9eefefbcfaec6ec93c68777dab29985
Jess Sickles <jasickle@umich.edu>
2017-12-06 16:37:41 -0500

    added json file and writing to database

--- 206finalproj.py
+++ 206finalproj.py
@@ -2,41 +2,55 @@ import requests
 import facebook
 import json
 import sqlite3
+from datetime import date
+import calendar
 
 access_token = None
 if access_token is None:
 	access_token = input("\n Copy and paste token from explorer: \n ")
 
 graph = facebook.GraphAPI(access_token)
-likes = graph.get_connections('me','likes') 
+likes = graph.get_connections('me','likes', limit=100) 
 profile = graph.get_object('me', fields = 'name,likes')
 #print(json.dumps(profile, indent = 4))
 #caching content into json file 
-
-i = 0
-while i < 4:
+likesList=[]
+while True:
+	try:
 		with open('206_FinalFB_cache.json','a') as f:
 				for like in likes['data']:
 					f.write(json.dumps(like)+"\n")	
+					likesList.append(like)
 				likes = requests.get(likes['paging']['next']).json()
-				i+=1
-				
-				
-				
+	except KeyError:
+	
+		#ran out of posts
+		break			
 
 #creating database 
-# conn =  sqlite3.connect('Fblikes.sqlite')
-# cursor = conn.cursor()
-# cursor.execute('DROP TABLE IF EXISTS Fblikes')
-# cursor.execute('CREATE TABLE Fblikes (post_name TEXT, post_ID TEXT, created_at TIMESTAMP )')
+conn =  sqlite3.connect('Fblikes.sqlite')
+cursor = conn.cursor()
+cursor.execute('DROP TABLE IF EXISTS Fblikes')
+cursor.execute('CREATE TABLE Fblikes (post_name TEXT, created_at TIMESTAMP, weekday TEXT )')
 
-# cache_file = open('206_FinalFB_cache.json','r')
-# cache_contents = cache_file.read()
-# cache_file.close()
-# CACHE_DICTION = json.loads(cache_contents)
-# for like in CACHE_DICTION:
-# 	tup = like["name"], like ["id"], like["created_at"]
-# 	cursor.execute('INSERT INTO FbLikes (post_name,post_ID,created_at) VALUES (?,?,?)',tup)
+for like in likesList:
+	tup = like["name"],like["created_time"]
+	cursor.execute('INSERT INTO FbLikes (post_name,created_at) VALUES (?,?)',tup)
+	conn.commit()
+
+
+
+def getDay():
+	time=[]
+	dateslist=[]
+	weekday=[]
+	for like in likesList:
+		time.append(like["created_time"])
+	for t in time:
+		dateslist.append(time.replace('-', ' ').replace('T', ' ').split())
+	for time in dateslist:
+		
+
 
 
 
